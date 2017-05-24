5393f26a086713470319964283de102e34591ea4
Sivan Mehta <Sivanmehta2@gmail.com>
2017-05-23 20:21:55 -0400

    Initial commit

---
+++ src/store/configure-store.js
@@ -0,0 +1,11 @@
+// import rootReducer from '../reducers'
+import {createStore} from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
+
+export default (initialState) => {
+  return createStore(
+    // rootReducer,
+    initialState,
+    devToolsEnhancer()
+  )
+}
