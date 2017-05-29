289f2d8a788a83c24fbefcc75b74af4752674822
Sivan Mehta <Sivanmehta2@gmail.com>
2017-05-29 17:13:04 -0400

    render fancy spinner

--- src/argument/index.js
+++ src/argument/index.js
@@ -11,33 +11,44 @@ export default class Argument extends React.Component {
 
     this.renderCategories = this.renderCategories.bind(this)
     this.fetchArgument = this.fetchArgument.bind(this)
-  }
-
-  fetchArgument() {
-    // would ideally hit the server, but we're not there yet
-    // the number of categories ideally would be something from 2 - 4
-    // so we can make a pretty number of columns.
-    //
-    // We also could potentially have more columns like
-    // Alternatives, (Worthwhile)ness, etc...
-    this.setState({data: ['Pros', 'Cons'] })
-
-    this.renderCategories()
+    this.renderSpinner = this.renderSpinner.bind(this)
   }
 
   componentDidMount() {
-    this.fetchArgument()
+    // simulate a slow server by delaying by a half second
+    setTimeout(this.fetchArgument, 1000)
+  }
+
+  fetchArgument() {
+    // would ideally hit the server and change the state
+    // accordingly, but we're not there yet
+    // We also could potentially have more columns like
+    // Alternatives, (Worthwhile)ness, etc...
+    this.setState({ data: ['Pros', 'Cons'] })
+
   }
 
   renderCategories() {
+    // the number of categories ideally would be something from 2 - 4
+    // so we can make a pretty number of columns.
+
     const colSize = 12 / this.state.data.length
-    return this.state.data.map(cat => (
+    return this.state.data ? this.state.data.map(cat => (
       <Col xs = { 12 } sm = { colSize } key = { cat + '-points' }>
         <Panel header = { cat } key = { cat + '-title' }>
           Panel content
         </Panel>
       </Col>
-    ))
+    )) : ''
+  }
+
+  renderSpinner() {
+
+    return (
+      <p className = 'text-center'>
+        Fetching data... <i className="fa fa-refresh fa-spin fa-fw"></i>
+      </p>
+    )
   }
 
   render() {
@@ -45,7 +56,7 @@ export default class Argument extends React.Component {
       <div>
         <h1> { this.props.match.params.id} </h1>
         <Row>
-          { this.renderCategories() }
+          {this.state.data ? this.renderCategories() : this.renderSpinner()}
         </Row>
       </div>
     )
---
+++ src/argument/list-of-points.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Col, Row, Panel } from 'react-bootstrap'
+
+export default class Points extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+  }
+}
