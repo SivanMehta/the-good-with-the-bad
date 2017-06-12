ea9a1252ab76bbe758c1b35a05ffc0c014fbd9fa
Sivan Mehta <Sivanmehta2@gmail.com>
2017-06-12 16:32:24 -0400

    broadcast messages to everyone

--- server/chatbot.js
+++ server/chatbot.js
@@ -1,15 +1,20 @@
-var WebSocketServer = require('ws').Server
-var wss = new WebSocketServer({ port: 8081 })
+var WebSocket = require('ws')
+var wss = new WebSocket.Server({ port: 8081 })
+
+var messages = []
+var i = 1
 
 exports.init = (cb) => {
   wss.on('connection', (ws) => {
-    var messages = []
-    var i = 1
 
     function changeState(message = i++, from = "Chatbot") {
       messages.unshift({message: message, from: from})
       messages = messages.slice(0, 10)
-      ws.send(JSON.stringify(messages))
+      wss.clients.forEach(client => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(messages))
+        }
+      })
     }
 
     ws.on('message', (message) => { changeState(message, "Browser") })
