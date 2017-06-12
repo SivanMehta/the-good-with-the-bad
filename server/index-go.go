58428973b57c39628d58ca4460b0b29497363173
Sivan Mehta <Sivanmehta2@gmail.com>
2017-06-12 19:40:34 -0400

    tabs --> spaces

--- server/index-go.go
+++ server/index-go.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"log"
-	"net/http"
+  "log"
+  "net/http"
   "encoding/json"
 
-	"github.com/go-ozzo/ozzo-routing"
-	"github.com/go-ozzo/ozzo-routing/access"
-	"github.com/go-ozzo/ozzo-routing/slash"
-	"github.com/go-ozzo/ozzo-routing/content"
-	"github.com/go-ozzo/ozzo-routing/fault"
-	"github.com/go-ozzo/ozzo-routing/file"
+  "github.com/go-ozzo/ozzo-routing"
+  "github.com/go-ozzo/ozzo-routing/access"
+  "github.com/go-ozzo/ozzo-routing/slash"
+  "github.com/go-ozzo/ozzo-routing/content"
+  "github.com/go-ozzo/ozzo-routing/fault"
+  "github.com/go-ozzo/ozzo-routing/file"
 
   "github.com/icrowley/fake"
 
@@ -39,21 +39,21 @@ func makePointArray(count int) []Point {
 }
 
 func main() {
-	router := routing.New()
+  router := routing.New()
 
-	router.Use(
-		// all these handlers are shared by every route
-		access.Logger(log.Printf),
-		slash.Remover(http.StatusMovedPermanently),
-		fault.Recovery(log.Printf),
-	)
+  router.Use(
+    // all these handlers are shared by every route
+    access.Logger(log.Printf),
+    slash.Remover(http.StatusMovedPermanently),
+    fault.Recovery(log.Printf),
+  )
 
-	api := router.Group("/api")
-	api.Use(
-		// these handlers are shared by the routes in the api group only
-		content.TypeNegotiator(content.JSON),
-	)
-	api.Get(`/<arg>`, func(c *routing.Context) error {
+  api := router.Group("/api")
+  api.Use(
+    // these handlers are shared by the routes in the api group only
+    content.TypeNegotiator(content.JSON),
+  )
+  api.Get(`/<arg>`, func(c *routing.Context) error {
     argument := &Arg{makePointArray(7), makePointArray(5)}
     data, err := json.Marshal(argument)
     if err != nil {
@@ -61,18 +61,18 @@ func main() {
     }
     c.Set("Content-Type", "application/json")
     return c.Write(string(data))
-	})
+  })
 
-	// serve files under the "public" subdirectory
-	router.Get("/build/*", file.Server(file.PathMap{
-		"/build": "/public/build",
-	}))
+  // serve files under the "public" subdirectory
+  router.Get("/build/*", file.Server(file.PathMap{
+    "/build": "/public/build",
+  }))
 
   // serve index file otherwise to allow
   // for client-side routing
   router.Get("/*", file.Content("public/index.html"))
 
-	http.Handle("/", router)
-	http.ListenAndServe(":8080", nil)
+  http.Handle("/", router)
+  http.ListenAndServe(":8080", nil)
 }
 //
