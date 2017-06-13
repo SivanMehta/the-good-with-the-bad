223e6f015056b663c6ae3121b84a10bd43051a0b
Sivan Mehta <Sivanmehta2@gmail.com>
2017-06-13 16:23:24 -0400

    make a 404 page

--- src/index.js
+++ src/index.js
@@ -10,6 +10,23 @@ import { Login, PrivateRoute } from './auth/components'
 
 const About = () => <p>Some really cool information</p>
 
+const Status = ({ code, children }) => (
+ <Route render={({ staticContext }) => {
+   if (staticContext)
+     staticContext.status = code
+   return children
+ }}/>
+)
+
+const NotFound = () => (
+  <Status code={404}>
+    <div>
+      <h1>¯\_(ツ)_/¯</h1>
+      <p>I have <a href = "https://youtu.be/qPefOfu2TIU?t=9">no idea</a> what this is.</p>
+    </div>
+  </Status>
+)
+
 render((
   <Router history={ browserHistory }>
     <div>
@@ -20,6 +37,7 @@ render((
           <PrivateRoute path = "/argument/:id" component = { Argument } />
           <Route path = "/login" component = { Login } />
           <Route exact path = "/" component = { About } />
+          <Route component = { NotFound } />
         </Switch>
       </div>
     </div>
