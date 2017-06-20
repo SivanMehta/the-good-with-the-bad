b8f0e629de562a6a7bafe3efcdc216be7ce42ffe
Sivan Mehta <Sivanmehta2@gmail.com>
2017-06-19 21:23:50 -0400

    centralize snarky status messages

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -14,10 +14,12 @@ type authorization struct {
 
 // Use a levelDB to store account information
 var accounts, _ = leveldb.OpenFile("data/", nil)
+var codes = make(map[int]string)
 
-func respond(c *routing.Context, header int, message string) error {
+func respond(c *routing.Context, header int) error {
+  var message []byte = []byte(codes[header])
   c.Response.WriteHeader(header)
-  c.Response.Write([]byte(message))
+  c.Response.Write(message)
   return nil
 }
 
@@ -30,11 +32,11 @@ func Register(c *routing.Context) error {
 
   exists, _ := accounts.Has([]byte(user.Username), nil)
   if(exists) {
-    return respond(c, 409, "Somebody beat you to it")
+    return respond(c, 409)
   }
 
   accounts.Put([]byte(user.Username), []byte(user.Password), nil)
-  return respond(c, 200, "Nice to Meet You!")
+  return respond(c, 201)
 }
 
 func Authorize(c *routing.Context) error {
@@ -48,14 +50,20 @@ func Authorize(c *routing.Context) error {
   if(authorized) {
     pass, _ := accounts.Get([]byte(user.Username), nil)
     if(string(pass) == user.Password) {
-      return respond(c, 200, "You're good!")
+      return respond(c, 200)
     }
   }
-  return respond(c, 403, "Who the h*ck are you?")
+  return respond(c, 403)
 }
 
 func Populate() {
   // populate with a bunch of fake accounts
   err :=  accounts.Put([]byte("sivan"), []byte("secret"), nil)
   if err != nil { panic(err) }
+
+  // add status codes with some snarky messages
+  codes[200] = "Welcome!"
+  codes[201] = "Nice to Meet You!"
+  codes[403] = "Who the h*ck are you?"
+  codes[409] = "Somebody beat you to it"
 }
